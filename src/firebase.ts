7b32a425eceef95d9c248e22f361693f3044de7e
ilakk manoharan <ilakkmanoharan@gmail.com>
2025-05-08 16:21:08 -0500

    Update firebase.ts
    Removed firebase project details

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,13 +3,13 @@ import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCm0sdz3d0_JNbKGd3IBEgPGNywuuf7aPk",
-  authDomain: "todo-ts-app-59ba0.firebaseapp.com",
-  projectId: "todo-ts-app-59ba0",
-  storageBucket: "YOUR_PROJECT_ID.appspot.todo-ts-app-59ba0.firebasestorage.app",
-  messagingSenderId: "712278425855",
-  appId: "1:712278425855:web:93c466eee1a8e8d2728cca",
-  measurementId: "G-SZZQXGQ7MX"
+  apiKey: "###############",
+  authDomain: "###############",
+  projectId: "###################",
+  storageBucket: "#########################",
+  messagingSenderId: "##########",
+  appId: "###########################",
+  measurementId: "#############"
 };
 
 const app = initializeApp(firebaseConfig);
